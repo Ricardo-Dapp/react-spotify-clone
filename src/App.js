7864a727818c7c0c2a1423d54a8a54c4114ac250
Ricardo Rodriguez <rvrodriguez2000@gmail.com>
2022-08-09 23:11:15 -0700

    added body,footer,sidebar...

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { useEffect, useState } from "react";
 import SpotifyWebApi from "spotify-web-api-js";
 import "./App.css";
 import Login from "./components/Login";
+import Player from "./components/Player";
 import { getTokenFromUrl } from "./spotify";
+import { useDataLayerValue } from "./DataLayer";
 
 const spotify = new SpotifyWebApi();
 
 function App() {
-  const [token, setToken] = useState(null);
+  const [{ user, token }, dispatch] = useDataLayerValue();
 
   useEffect(() => {
     const hash = getTokenFromUrl();
@@ -16,19 +18,28 @@ function App() {
     const _token = hash.access_token;
 
     if (_token) {
-      setToken(_token);
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      });
 
       spotify.setAccessToken(_token);
 
       spotify.getMe().then((user) => {
         console.log("Person", user);
+
+        dispatch({
+          type: "SET_USER",
+          user: user,
+        });
       });
     }
     console.log("Token", _token);
   }, []);
-  return (
-    <div className="app">{token ? <h1>I am logged in</h1> : <Login />}</div>
-  );
+
+  console.log("user:", user);
+  console.log("token:", token);
+  return <div className="app">{token ? <Player /> : <Login />}</div>;
 }
 
 export default App;
---
+++ src/reducer.js
@@ -0,0 +1,32 @@
+export const initialState = {
+  user: null,
+  playlists: [],
+  playing: false,
+  item: null,
+  token:
+    "BQBqAme-1x2k0pcNqR21d8ySso039n4IaMbdQElZfJeCEiSPwJJTFcJi8oX6s7SV1gAaAoKVviZqJ03sD-7-Rb-OGx0RIrgZANWfuwI39arAYja0z6iDXt0FndT1InYgS223Gb1X7SWLq7rCndwUColkYFkKskmICW8EEA229cmYbGe6_bROnl6gFjB6n5K3tdbo2oY",
+};
+
+// Remove token sting and set to null once finished developing
+
+const reducer = (state, action) => {
+  console.log(action);
+
+  switch (action.type) {
+    case "SET_USER":
+      return {
+        ...state,
+        user: action.user,
+      };
+
+    case "SET_TOKEN":
+      return {
+        ...state,
+        token: action.token,
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
